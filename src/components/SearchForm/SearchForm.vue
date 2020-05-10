f55514211f766a7390baf52a6aa4913a8edfc360
Eli.zhu <58264585+Yongtian-zhu@users.noreply.github.com>
2020-05-11 00:25:29 +0800

    V2测试

--- src/components/SearchForm/SearchForm.vue
+++ src/components/SearchForm/SearchForm.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="search-form">
     <a-fragment>
-      <div class="tableListForm">{{renderForm()}}</div>
+      <div class="tableListForm">
+        <a-input type="text" />
+      </div>
       <div class="tableListOperator">
-        <div class="tableListleft">
-          {this.$attrs.rightButton}
-        </div>
+        <div class="tableListleft">2222</div>
         <div class="tableListRight">
-          <a-button type="primary" @click="handleSearch">
-            {this.$attrs.SearchText || "查询"}
-          </a-button>
-          <a-button style=" marginLeft: 8" @click="handleFormReset">
-            重置
-          </a-button>
+          <a-button type="primary" @click="handleSearch">查询</a-button>
+          <a-button @click="handleFormReset">重置</a-button>
         </div>
       </div>
     </a-fragment>
@@ -20,30 +16,28 @@
 </template>
 
 <script>
-  // 引入文件, 如:import 组件名 from '组件路径'
-  // import { connect } from '@/dva'
-  import { Form, Row, Col, Select, Icon, Input, DatePicker, InputNumber } from 'antd'
-  import '@/commonTableList.less'
+// 引入文件, 如:import 组件名 from '组件路径'
+// import { connect } from '@/dva'
+// import { Form, Row, Col, Select, Icon, Input, DatePicker, InputNumber } from 'antd'
+// import '@/commonTableList.less'
 
-  export default {
-    // import需要注入到对象中才能使用
-    components: {
+export default {
+  name: 'SearchForm',
+  // import需要注入到对象中才能使用
+  components: {},
+  data () {
+    // 存放数据
+    return {}
+  },
+  // 监听属性
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 方法集合
 
-    },
-    data () { // 存放数据
-      return {
-
-      }
-    },
-    // 监听属性
-    computed: {},
-    // 监控data中的数据变化
-    watch: {},
-    // 方法集合
-
-    methods: {
-      handleSearch (e) {
-        e.preventDefault()
+  methods: {
+    handleSearch (e) {
+      e.preventDefault()
       this.$attrs.form.validateFields((err, fieldsValue) => {
         if (err) return
         const values = { ...fieldsValue }
@@ -56,10 +50,7 @@
     },
 
     handleFormReset () {
-      const {
-        form,
-        dispatch
-      } = this.$attrs
+      const { form, dispatch } = this.$attrs
       form.resetFields()
       this.formValues = {}
       dispatch({
@@ -74,9 +65,15 @@
 
       switch (type) {
         case 'number':
-          return <InputNumber placeholder="请输入" min={0} style={{
-            width: '100%'
-          }} />
+          return (
+            <InputNumber
+              placeholder="请输入"
+              min={0}
+              style={{
+                width: '100%'
+              }}
+            />
+          )
         // break;
 
         case 'text':
@@ -84,39 +81,69 @@
         // break;
 
         case 'date_picker':
-          return <DatePicker placeholder="请选择" allowClear={false} style={{
-            width: '100%'
-          }} />
+          return (
+            <DatePicker
+              placeholder="请选择"
+              allowClear={false}
+              style={{
+                width: '100%'
+              }}
+            />
+          )
         // break;
 
         case 'range_picker':
-          return <RangePicker style={{
-            width: '100%'
-          }} allowClear={false} />
+          return (
+            <RangePicker
+              style={{
+                width: '100%'
+              }}
+              allowClear={false}
+            />
+          )
         // break;
 
         case 'select_multiple':
-          return <Select mode="multiple" placeholder={placeholder} style={{
-            width: '100%'
-          }}>
-            {item.options.map((option, index) => {
-              return <Option key={index} value={option.value}>
-                  {option.text}
-                </Option>
-            })}
-          </Select>
+          return (
+            <Select
+              mode="multiple"
+              placeholder={placeholder}
+              style={{
+                width: '100%'
+              }}
+            >
+              {item.options.map((option, index) => {
+                return (
+                  <Option key={index} value={option.value}>
+                    {option.text}
+                  </Option>
+                )
+              })}
+            </Select>
+          )
 
         case 'select':
-          return <Select placeholder="请选择" showSearch optionFilterProp="children" filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0} style={{
-            width: '100%'
-          }}>
-            {}
-            {item.options && item.options.map((option, index) => {
-              return <Option key={index} value={option.value}>
-                    {option.value}
-                  </Option>
-            })}
-          </Select>
+          return (
+            <Select
+              placeholder="请选择"
+              showSearch
+              optionFilterProp="children"
+              filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+              style={{
+                width: '100%'
+              }}
+            >
+              {}
+              {item.options &&
+                item.options.map((option, index) => {
+                  return (
+                    <Option key={index} value={option.value}>
+                      {option.value}
+                    </Option>
+                  )
+                })}
+            </Select>
+          )
 
         default:
           return <Input placeholder="请选择" autoComplete="off" />
@@ -125,110 +152,118 @@
     },
 
     toggleForm () {
-      const {
-        expandForm
-      } = this.$data
+      const { expandForm } = this.$data
       this.expandForm = !expandForm
     },
 
     renderSimpleForm () {
       const {
-        form: {
-          getFieldDecorator
-        },
+        form: { getFieldDecorator },
         formList
       } = this.$attrs
       const simpleForm = formList.slice(0, 3)
-      return <Form onSubmit={this.handleSearch} layout="inline">
-        <Row gutter={{
-          md: 7,
-          lg: 24,
-          xl: 48
-        }}>
-          {simpleForm.map((item, index) => {
-            return <Col md={7} sm={24} key={index}>
-                <FormItem label={<span class="labelText">{item.label}</span>}>
-                  {getFieldDecorator(item.field)(this.switchItem(item))}
-                </FormItem>
+      return (
+        <Form onSubmit={this.handleSearch} layout="inline">
+          <Row
+            gutter={{
+              md: 7,
+              lg: 24,
+              xl: 48
+            }}
+          >
+            {simpleForm.map((item, index) => {
+              return (
+                <Col md={7} sm={24} key={index}>
+                  <FormItem label={<span class="labelText">{item.label}</span>}>
+                    {getFieldDecorator(item.field)(this.switchItem(item))}
+                  </FormItem>
+                </Col>
+              )
+            })}
+            {formList.length > 3 ? (
+              <Col md={3} sm={24}>
+                <span class="submitButtons">
+                  <a onClick={this.toggleForm}>
+                    展开 <Icon type="down" />
+                  </a>
+                </span>
               </Col>
-          })}
-          {formList.length > 3 ? <Col md={3} sm={24}>
-            <span class="submitButtons">
-              <a onClick={this.toggleForm}>
-                展开 <Icon type="down" />
-              </a>
-            </span>
-          </Col> : ' ' }
-        </Row>
-      </Form>
+            ) : (
+              ' '
+            )}
+          </Row>
+        </Form>
+      )
     },
 
     renderAdvancedForm () {
       const {
-        form: {
-          getFieldDecorator
-        },
+        form: { getFieldDecorator },
         formList
       } = this.$attrs
       const simpleForm = formList.slice(0, 3)
       const AdvancedForm = formList.slice(3, formList.length)
-      return <Form onSubmit={this.handleSearch} layout="inline">
-        <Row gutter={{
-          md: 7,
-          lg: 24,
-          xl: 48
-        }}>
-          {simpleForm.map((item, index) => {
-            return <Col md={7} sm={24} key={index}>
-                <FormItem label={<span class="labelText">{item.label}</span>}>
-                  {getFieldDecorator(item.field)(this.switchItem(item))}
-                </FormItem>
-              </Col>
-          })}
+      return (
+        <Form onSubmit={this.handleSearch} layout="inline">
+          <Row
+            gutter={{
+              md: 7,
+              lg: 24,
+              xl: 48
+            }}
+          >
+            {simpleForm.map((item, index) => {
+              return (
+                <Col md={7} sm={24} key={index}>
+                  <FormItem label={<span class="labelText">{item.label}</span>}>
+                    {getFieldDecorator(item.field)(this.switchItem(item))}
+                  </FormItem>
+                </Col>
+              )
+            })}
 
-          <Col md={3} sm={24}>
-            <span class="submitButtons">
-              <a style={{
-                marginLeft: 8
-              }} onClick={this.toggleForm}>
-                收起 <Icon type="up" />
-              </a>
-            </span>
-          </Col>
-        </Row>
-        <Row gutter={{
-          md: 7,
-          lg: 24,
-          xl: 48
-        }}>
-          {AdvancedForm.map((item, index) => {
-            return <Col md={7} sm={24} key={index}>
-                <FormItem label={<span class="labelText">{item.label}</span>}>
-                  {getFieldDecorator(item.field)(this.switchItem(item))}
-                </FormItem>
-              </Col>
-          })}
-        </Row>
-      </Form>
+            <Col md={3} sm={24}>
+              <span class="submitButtons">
+                <a
+                  style={{
+                    marginLeft: 8
+                  }}
+                  onClick={this.toggleForm}
+                >
+                  收起 <Icon type="up" />
+                </a>
+              </span>
+            </Col>
+          </Row>
+          <Row
+            gutter={{
+              md: 7,
+              lg: 24,
+              xl: 48
+            }}
+          >
+            {AdvancedForm.map((item, index) => {
+              return (
+                <Col md={7} sm={24} key={index}>
+                  <FormItem label={<span class="labelText">{item.label}</span>}>
+                    {getFieldDecorator(item.field)(this.switchItem(item))}
+                  </FormItem>
+                </Col>
+              )
+            })}
+          </Row>
+        </Form>
+      )
     },
 
     renderForm () {
-      const {
-        expandForm
-      } = this.$data
+      const { expandForm } = this.$data
       return expandForm ? this.renderAdvancedForm() : this.renderSimpleForm()
     }
-  },
-
-    // 声明周期 - 创建完成(可访问当前this实例)
-    created () {},
-    // 声明周期 - 挂载完成(可访问DOM元素)
-    mounted () {}
   }
-
+}
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 /* @import url(); 引入样式 */
-
 </style>
--- src/views/form/BasicForm.vue
+++ src/views/form/BasicForm.vue
@@ -1,11 +1,12 @@
 <template>
   <a-card :bordered="false">
+    <search-form />
     <div class="table-page-search-wrapper">
       <a-form layout="inline">
         <a-row :gutter="48">
           <a-col :md="8" :sm="24">
             <a-form-item label="组织名称">
-              <a-input placeholder="请输入"/>
+              <a-input placeholder="请输入" />
             </a-form-item>
           </a-col>
           <a-col :md="8" :sm="24">
@@ -36,25 +37,24 @@
       </a-form>
     </div>
 
-    <s-table
-      ref="table"
-      size="default"
-      :columns="columns"
-      :data="loadData"
-    >
-      <div
-        slot="expandedRowRender"
-        slot-scope="record"
-        style="margin: 0">
-        <a-row
-          :gutter="24"
-          :style="{ marginBottom: '12px' }">
-          <a-col :span="12" v-for="(role, index) in record.permissions" :key="index" :style="{ marginBottom: '12px' }">
+    <s-table ref="table" size="default" :columns="columns" :data="loadData">
+      <div slot="expandedRowRender" slot-scope="record" style="margin: 0">
+        <a-row :gutter="24" :style="{ marginBottom: '12px' }">
+          <a-col
+            :span="12"
+            v-for="(role, index) in record.permissions"
+            :key="index"
+            :style="{ marginBottom: '12px' }"
+          >
             <a-col :span="4">
               <span>{{ role.permissionName }}：</span>
             </a-col>
             <a-col :span="20" v-if="role.actionEntitySet.length > 0">
-              <a-tag color="cyan" v-for="(action, k) in role.actionEntitySet" :key="k">{{ action.describe }}</a-tag>
+              <a-tag
+                color="cyan"
+                v-for="(action, k) in role.actionEntitySet"
+                :key="k"
+              >{{ action.describe }}</a-tag>
             </a-col>
             <a-col :span="20" v-else>-</a-col>
           </a-col>
@@ -65,15 +65,10 @@
         <a-divider type="vertical" />
         <a-dropdown>
           <a class="ant-dropdown-link">
-            更多 <a-icon type="down" />
+            更多
+            <a-icon type="down" />
           </a>
           <a-menu slot="overlay">
-            <a-menu-item>
-              <a href="javascript:;">详情</a>
-            </a-menu-item>
-            <a-menu-item>
-              <a href="javascript:;">禁用</a>
-            </a-menu-item>
             <a-menu-item>
               <a href="javascript:;">删除</a>
             </a-menu-item>
@@ -83,11 +78,11 @@
     </s-table>
 
     <role-modal ref="modal" @ok="handleOk"></role-modal>
-
   </a-card>
 </template>
 
 <script>
+import SearchForm from '@/components/SearchForm/SearchForm'
 import { STable } from '@/components'
 import RoleModal from './modules/RoleModal'
 
@@ -95,11 +90,13 @@ export default {
   name: 'BaseForm',
   components: {
     STable,
-    RoleModal
+    RoleModal,
+    SearchForm
   },
   data () {
     return {
-      description: '列表使用场景：后台管理中的权限管理以及角色管理，可用于基于 RBAC 设计的角色权限控制，颗粒度细到每一个操作类型。',
+      description:
+        '列表使用场景：后台管理中的权限管理以及角色管理，可用于基于 RBAC 设计的角色权限控制，颗粒度细到每一个操作类型。',
 
       visible: false,
 
@@ -128,7 +125,8 @@ export default {
           title: '创建时间',
           dataIndex: 'createTime',
           sorter: true
-        }, {
+        },
+        {
           title: '操作',
           width: '150px',
           dataIndex: 'action',
@@ -137,11 +135,13 @@ export default {
       ],
       // 加载数据方法 必须为 Promise 对象
       loadData: parameter => {
-        return this.$http.get('/role', {
-          params: Object.assign(parameter, this.queryParam)
-        }).then(res => {
-          return res.result
-        })
+        return this.$http
+          .get('/role', {
+            params: Object.assign(parameter, this.queryParam)
+          })
+          .then(res => {
+            return res.result
+          })
       },
 
       selectedRowKeys: [],
